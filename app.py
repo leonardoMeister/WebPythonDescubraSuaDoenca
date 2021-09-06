10ccbc69d48e65012088615f16facc6eb3df0478
leonardo Meister <leozinhomm1@gmail.com>
2021-09-06 15:24:30 -0300

    Inicio, menu, Doenças.

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-from flask import Flask
+from flask import Flask , render_template, url_for 
+from dir_home.home import bp_home
+from dir_doencas.doencas import bp_doenca
 
-app = Flask(__name__)
+app = Flask(__name__) 
 
-@app.route("/")
-def rotaHome():
-    return "<h1> Hello word </h1>" , 200
+app.register_blueprint(bp_home)
+app.register_blueprint(bp_doenca)
+
+
+@app.errorhandler(404)
+def rotaErro404(error):
+    return render_template("form_404.html"), 404
 
 
 if __name__ == "__main__":
-    app.run(debug=True , port= 5000)
+    app.run(debug= True, port= 5000)
