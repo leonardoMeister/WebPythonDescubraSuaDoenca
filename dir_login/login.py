051ac47a74b9173ddfc39f62c084c62745205e74
leonardo Meister <leozinhomm1@gmail.com>
2021-09-20 14:56:32 -0300

    Add de sessions

--- dir_login/login.py
+++ dir_login/login.py
@@ -1,5 +1,4 @@
-from types import MethodType
-from flask import Blueprint, render_template , redirect , request, url_for
+from flask import Blueprint, render_template , redirect , request, url_for, session
 
 bp_login = Blueprint('login',__name__, url_prefix="/", template_folder= 'templates')
 
@@ -14,11 +13,20 @@ def validarLogin():
         senha = request.form['senha']
 
     if (nome == "admin" and senha == "admin"):
+        #LIMPANDO E ADICIONANDO A SESSÃO O USUARIO
+        session.clear()
+        session['nome'] = nome
         return redirect(url_for("home.rotaHome"))
     else :
         return redirect(url_for('login.login' , falhalogin =1))
 
 
 @bp_login.route('/logoff' , methods = ['GET'])
-def validarLogoff():
+def logoff():
+
+    #SERVE PARA LIMPAR UM VALOR INDIVIDUAL
+    session.pop('nome' , None)
+    #LIMPA TODA A SESSION
+    session.clear()
+
     return redirect(url_for('login.login'))
