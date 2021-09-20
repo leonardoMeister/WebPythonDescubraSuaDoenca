f078fce80f53323e830c59956637943f7fa5fbfa
leonardo Meister <leozinhomm1@gmail.com>
2021-09-20 14:12:17 -0300

    Criação de login engessado.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask , render_template, url_for 
+from flask import Flask , render_template, url_for , redirect, request
 from dir_home.home import bp_home
 from dir_doencas.doencas import bp_doenca
 from dir_OqueEuTenho.OqueEuTenho import bp_OqueEuTenho
@@ -6,9 +6,11 @@ from dir_acompanhamento.acompanhamento import bp_acompanhamento
 from dir_agendamento.agendamento import bp_agendamento
 from dir_meusGraficos.meusGraficos import bp_meusGraficos
 from dir_laudo.laudo import bp_laudo
+from dir_login.login import bp_login
 
 app = Flask(__name__) 
 
+app.register_blueprint(bp_login)
 app.register_blueprint(bp_laudo)
 app.register_blueprint(bp_meusGraficos)
 app.register_blueprint(bp_agendamento)
--- dir_home/home.py
+++ dir_home/home.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template , request
 
 bp_home = Blueprint('home',__name__, url_prefix="/", template_folder= 'templates')
 
-@bp_home.route("/")
+@bp_home.route("/home")
 def rotaHome():
     return render_template("home.html")
 
