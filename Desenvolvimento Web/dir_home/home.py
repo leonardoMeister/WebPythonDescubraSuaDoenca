cbc909b957b2345f68c28cbf5ba84458ce0eff05
leonardo Meister <leozinhomm1@gmail.com>
2021-10-28 21:11:43 -0300

    Mudanca de Visual Total, Reformulacao Visual.

--- Desenvolvimento Web/dir_home/home.py
+++ Desenvolvimento Web/dir_home/home.py
@@ -1,14 +1,9 @@
 from flask import Blueprint, render_template , request
-from dir_login.login import validarSessao
+#from dir_login.login import validarSessao
 
 bp_home = Blueprint('home',__name__, url_prefix="/", template_folder= 'templates')
 
 @bp_home.route("/home")
-@validarSessao
+#@validarSessao
 def rotaHome():
     return render_template("home.html")
-
-@bp_home.route("/usuario")
-@validarSessao
-def rotaHomeUsuario():
-    return render_template("homeUsuario.html")
